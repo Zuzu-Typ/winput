e862395d191cb00d0b089d3fb813eda0dcdb5346
Zuzu-Typ <Zuzu.Typ@gmail.com>
2022-03-15 23:14:00 +0100

    Bump version (1.4.0)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='winput',
     
-    version='1.3.0',
+    version='1.4.0',
 
     description='Capture and send keyboard and mouse input',
     
